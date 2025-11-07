eadf960b5bdaa7dff6d95fb358acceeaaad07959
ramprasad9182 <ramprasad@nhclindia.com>
2025-11-07 18:07:57 +0530

    07-11-25

---
+++ custom_addons-74-07-11-25/rental_management/models/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020-Today TechKhedut.
+# Part of TechKhedut. See LICENSE file for full copyright and licensing details.
+from . import property
+from . import user_type
+from . import tenancy
+from . import rent_invoice
+from . import property_maintenance
+from . import property_vendor
+from . import res_config
+from . import property_crm
